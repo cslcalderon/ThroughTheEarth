6be376abf928d3653914bd720bfee61457995b57
cslcalderon <sofiecal1228@gmail.com>
2018-06-06 21:30:17 -0500

    testing dummy change to test git

--- Assets/State.cs
+++ Assets/State.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 [CreateAssetMenu (menuName = "State")]
 public class State : ScriptableObject
 {
+    //do this to you do stuff
     [TextArea (14,10)] [SerializeField] string storyText;
 
     public string GetStateStory()
     {
         return storyText;
     }
-
-
 }
